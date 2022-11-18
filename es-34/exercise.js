d60de665d36283c5172627f25cc0e064d5d8ed17
Leonardo Cahuas <leonardocahuas@Leonardos-MacBook-Pro.local>
2022-11-18 18:45:13 +0100

    ex34

---
+++ es-34/exercise.js
@@ -0,0 +1,9 @@
+function printHello(){
+    console.log("hello")
+}
+function printAsyncName(callback,  name ){
+    
+    setTimeout(callback, 1000);
+    setTimeout(() => console.log(name), 2000);
+}
+printAsyncName(printHello, "Leonardo");
