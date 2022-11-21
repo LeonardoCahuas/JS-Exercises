834d98f3e98e784720c954a3bd14469bf5d2e06d
Leonardo Cahuas <leonardocahuas@Leonardos-MacBook-Pro.local>
2022-11-21 11:57:58 +0100

    ex38

---
+++ es-38/exercise.js
@@ -0,0 +1,29 @@
+let isLoggedIn = true;
+//async function promise(isLoggedIn){
+//    return new Promise((resolve, reject) => {
+let promise = new Promise((resolve, reject) => {
+        if (isLoggedIn === true){
+            resolve(Math.random())
+        } else{
+            reject(new Error("not logged in"))
+        }
+    })
+
+
+async function verifyNumber(){
+    let numb = await promise;
+    return new Promise((resolve, reject) => {
+        if(numb > 0.5){
+            resolve({name: "John", age: 24})
+        }else{
+            reject(new Error("low number"))
+        }
+    })
+}
+
+promise
+   .then((val) => console.log(val))   
+   .then(verifyNumber)
+   .then((obj) => console.log(obj)) 
+   .catch(() => console.error("there was an error"))
+
