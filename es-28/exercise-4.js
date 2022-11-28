d012607c9b93ec62e306ec0bce89be405637cade
Leonardo Cahuas <leonardocahuas@Leonardos-MacBook-Pro.local>
2022-11-28 18:15:12 +0100

    ex28 correction

--- es-28/exercise-4.js
+++ es-28/exercise-4.js
@@ -1,18 +1,6 @@
-function sum(...numbers) {
-  return numbers.reduce((sum, number)=>sum+number);
-}
-
-const numbers = [1, 2, 3];
-console.log(sum(numbers));
-
-/*
-
-OPPURE
-
 function sum(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
 const numbers = [1, 2, 3];
 console.log(sum(...numbers));
-*/
