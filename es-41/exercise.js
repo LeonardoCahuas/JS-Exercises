9380c5f5527b57c54428a6b8226fd5dd7c11b2f9
Leonardo Cahuas <leonardocahuas@Leonardos-MacBook-Pro.local>
2022-11-28 17:05:17 +0100

    ex41

---
+++ es-41/exercise.js
@@ -0,0 +1,14 @@
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+localStorage.setItem('user', JSON.stringify(user));
+
+loadStorage();
+
+let data = JSON.parse(localstorage.getItem("user"));
+
+console.log(data);
+
